3ac69fa69f163d0aa9159390356d7413d51376f9
S.Botchorishvili <S.Botchorishvili@tbcpay.ge>
2022-05-12 17:14:47 +0400

    gh-pages

---
+++ pages/contact.ce1c0d1e.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".contact").forEach((e=>{e.addEventListener("mouseenter",(()=>{e.firstElementChild.style.opacity="100%"})),e.addEventListener("mouseleave",(()=>{e.firstElementChild.style.opacity="70%"}))}));
+//# sourceMappingURL=contact.ce1c0d1e.js.map

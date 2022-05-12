2dd53e1e302be1a736f782f0115a89ee1f2cdec7
S.Botchorishvili <S.Botchorishvili@tbcpay.ge>
2022-05-12 16:12:32 +0400

    search logic done

--- dist/pages/flags.739bf03c.js
+++ dist/pages/flags.739bf03c.js
@@ -529,6 +529,8 @@ function hmrAcceptRun(bundle, id) {
 const url = "https://restcountries.com/v2/all";
 // DOM elements
 const countriesEl = document.getElementById("countries");
+const searchEl = document.getElementById("search");
+const filterEl = document.getElementById("filter");
 const getCountries = async ()=>{
     const res = await fetch(url);
     const data = await res.json();
@@ -545,7 +547,7 @@ const displayCountries = (data)=>{
         <img src="${item.flag}" alt="${item.name} flag" />
       </div>
       <div class="p-5">
-        <h2 class="font-bold text-lg">${item.name}</h2>
+        <h2 class="font-bold text-lg heading">${item.name}</h2>
         <p class="text-sm">
           <strong>Population:</strong>${item.population}
         </p>
@@ -559,6 +561,18 @@ const displayCountries = (data)=>{
         countriesEl.appendChild(countryEl);
     });
 };
+searchEl.addEventListener("input", (e)=>{
+    const value = e.target.value;
+    const countryNames = document.querySelectorAll(".heading");
+    countryNames.forEach((name)=>{
+        if (name.innerText.toLowerCase().includes(value.toLowerCase())) name.parentElement.parentElement.style.display = "block";
+        else name.parentElement.parentElement.style.display = "none";
+    });
+});
+filterEl.addEventListener("click", (e)=>{
+    const filterList = document.getElementById("filter-list");
+    filterList.classList.toggle("hidden");
+});
 
 },{}]},["ljsGX","ebWYT"], "ebWYT", "parcelRequire7e0d")
 
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ const url = "https://restcountries.com/v2/all";
 // DOM elements
 
 const countriesEl = document.getElementById("countries");
+const searchEl = document.getElementById("search");
+const filterEl = document.getElementById("filter");
 
 const getCountries = async () => {
   const res = await fetch(url);
@@ -29,7 +31,7 @@ const displayCountries = (data) => {
         <img src="${item.flag}" alt="${item.name} flag" />
       </div>
       <div class="p-5">
-        <h2 class="font-bold text-lg">${item.name}</h2>
+        <h2 class="font-bold text-lg heading">${item.name}</h2>
         <p class="text-sm">
           <strong>Population:</strong>${item.population}
         </p>
@@ -44,3 +46,22 @@ const displayCountries = (data) => {
     countriesEl.appendChild(countryEl);
   });
 };
+
+searchEl.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  const countryNames = document.querySelectorAll(".heading");
+
+  countryNames.forEach((name) => {
+    if (name.innerText.toLowerCase().includes(value.toLowerCase())) {
+      name.parentElement.parentElement.style.display = "block";
+    } else {
+      name.parentElement.parentElement.style.display = "none";
+    }
+  });
+});
+
+filterEl.addEventListener("click", (e) => {
+  const filterList = document.getElementById("filter-list");
+  filterList.classList.toggle("hidden");
+});
